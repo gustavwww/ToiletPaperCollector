de5e255ddc469ee6e36928f7008f7732a9a4328a
gustavwww <gustav.wadstrom@telia.com>
2021-05-15 14:54:22 +0200

    Changed way of emptying box

---
+++ Assets/Scripts/Controller/StoreController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Controller {
+    
+    public class StoreController : MonoBehaviour {
+
+
+        public Canvas storeCanvas;
+        public Canvas menuCanvas;
+        
+        public void backPressed() {
+            storeCanvas.gameObject.SetActive(false);
+            menuCanvas.gameObject.SetActive(true);
+        }
+        
+        
+    }
+}
--- Assets/Scripts/View/DestroyBox.cs
+++ Assets/Scripts/View/DestroyBox.cs
@@ -1,14 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using Model;
+using UnityEngine;
 
-public class DestroyBox : MonoBehaviour
-{
+namespace View {
+    public class DestroyBox : MonoBehaviour, ModelListener {
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Entity")) {
-            Destroy(other.gameObject);
-        }
+        public GameModel gameModel;
         
-    }
+        public float timeToDestroy = 3.0f;
+        private bool destroying = false;
 
+        private void OnTriggerStay(Collider other) {
+            if (other.CompareTag("Entity") && destroying) {
+                Destroy(other.gameObject);
+            }
+        }
+
+        private float timeLeft;
+
+        private void Start() {
+            gameModel.addListener(this);
+            timeLeft = timeToDestroy;
+        }
+
+        private void Update() {
+            if (destroying) {
+                timeLeft -= Time.deltaTime;
+                if (timeLeft <= 0.0f) {
+                    destroying = false;
+                    timeLeft = timeToDestroy;
+                }
+            }
+        }
+
+        public void boxFull() {
+            destroying = true;
+        }
+
+        public void levelUpdated(GameLevel level) {
+        }
+    }
+    
 }
+
